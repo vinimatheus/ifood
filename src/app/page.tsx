f7a7ca551975ae23a7346d73e173b8d871b34644
vinimatheus <vinimatheus999@gmail.com>
2024-04-29 20:51:37 -0300

    feat: add search components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/header";
+import Search from "@/components/header/search";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
+      <div className="px-5 py-4">
+        <Search />
+      </div>
     </main>
   );
 }
